c4708fb83355219a6f42b1b896272571311c0e4c
Magnus Beckett <mbalabix@gmail.com>
2024-08-14 23:23:44 +0000

    Update file waifu_perks.ts

--- src/data/waifu_perks.ts
+++ src/data/waifu_perks.ts
@@ -1761,7 +1761,7 @@ export const waifu_perks: WaifuPerk[] = [
     ],
     tier: 8,
     image: 'https://i.imgur.com/xapAe6N.png?1',
-    cost: 198,
+    cost: 190,
     desc: 'Sakura or Tsubone is instead the Lancer-class Pseudo- Servant Parvati, the Hindu goddess and wife of Shiva. If you purchase or capture the intended host first and apply this perk later, Parvati will keep her host’s memories and a blend of both host and Servant’s personalities, as if she had taken Substitute into her host’s life; if you purchase the host and this perk simultaneously, you will only get Parvati. This perk may not be applied to the same Sakura as Tainted Vessel, Kama Sammohana, Undivided, or Knight of the Cherry Blossom. You can only apply this perk to one of human Sakura, AI Sakura, or Tsubone at a time.',
   },
   {
