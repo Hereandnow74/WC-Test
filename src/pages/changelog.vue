d626df0cf1de613c92f537e77f85b00d11a2c44c
Alex A <sanekenator@gmail.com>
2024-08-14 19:22:28 +0500

    changelog update

--- src/pages/changelog.vue
+++ src/pages/changelog.vue
@@ -6,6 +6,16 @@
       </h2>
       <div>
         <h3 class="text-lg font-semibold text-center py-2 text-orange-700 dark:text-orange-300">
+          14 August 2024
+        </h3>
+        <ul class="list-decimal list-inside">
+          <li>New perks and changes to the rules to be more up to date. Work done by DepressedAlucard on GitLab.</li>
+          <li>~65 new user submitted characters, new missions, new worlds</li>
+          <li>Bugfixes & small changes</li>
+        </ul>
+      </div>
+      <div>
+        <h3 class="text-lg font-semibold text-center py-2 text-orange-700 dark:text-orange-300">
           09 August 2024
         </h3>
         <ul class="list-decimal list-inside">
